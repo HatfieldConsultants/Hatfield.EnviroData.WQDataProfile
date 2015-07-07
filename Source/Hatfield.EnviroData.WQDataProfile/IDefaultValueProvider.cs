26276b1b81403abee06dc02c90e0094f3830c93e
Daniel <luzhanxun.daniel@gmail.com>
2015-07-06 18:27:17 -0700

    Add Hatfield.EnviroData.WQDataProfile
    Add Hatfield.EnviroData.WQDataProfile

---
+++ Source/Hatfield.EnviroData.WQDataProfile/IDefaultValueProvider.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hatfield.EnviroData.WQDataProfile
+{
+    public interface IDefaultValueProvider
+    {
+        string Name { get; }
+
+        //Person Default Values
+        string DefaultPersonFirstName { get; }
+        string DefaultPersonMiddleName { get; }
+        string DefaultPersonLastName { get; }
+
+        //Organization Default Values
+        string DefaultOrganizationTypeCV { get; }
+        string DefaultOrganizationName { get; }
+        string DefaultOrganizationCode { get; }
+
+        //Processing Level Default Values
+        string DefaultProcessingLevels { get; }
+
+        //Sampling Features Default Values
+        string DefaultSamplingFeatureUUID { get; }
+        string DefaultSamplingFeatureTypeCV { get; }
+        string DefaultSamplingFeatureCode { get; }
+
+        //Method Default Values
+        string DefaultMethodTypeCV { get; }
+        string DefaultMethodCode { get; }
+        string DefaultMethodName { get; }
+        string DefaultMethodDescription { get; }
+        string DefaultMethodLink { get; }
+        string DefaultMethodOrganizationID { get; }
+
+        //Variable Default Values
+        string DefaultVariableTypeCV { get; }
+        string DefaultVariableCode { get; }
+        string DefaultVariableNameCV { get; }
+        string DefaultVariableDefinition { get; }
+        string DefaultVariableSpeciationCV { get; }
+        string DefaultVariableNoDataValue { get; }
+
+        //Units Default Values
+        string DefaultUnitsTypeCV { get; }
+        string DefaultUnitsAbbreviation { get; }
+        string DefaultUnitsName { get; }
+        string DefaultUnitsLink { get; }
+
+        //CV Default Values
+        string DefaultCVUnitsType { get; }
+        string DefaultCVTerm { get; }
+        string DefaultCVName { get; }
+    }
+}
---
+++ Source/Hatfield.StaticWQValueProvider/StaticDefaultValueProvider.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Hatfield.EnviroData.WQDataProfile;
+
+namespace Hatfield.StaticWQValueProvider
+{
+    public class StaticDefaultValueProvider : IDefaultValueProvider
+    {
+        public string Name
+        {
+            get {
+                return "Static Water Quality Default Value Provider";
+            }
+        }
+
+        public string DefaultPersonFirstName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultPersonMiddleName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultPersonLastName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultOrganizationTypeCV
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultOrganizationName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultOrganizationCode
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultProcessingLevels
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultSamplingFeatureUUID
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultSamplingFeatureTypeCV
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultSamplingFeatureCode
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultMethodTypeCV
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultMethodCode
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultMethodName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultMethodDescription
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultMethodLink
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultMethodOrganizationID
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultVariableTypeCV
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultVariableCode
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultVariableNameCV
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultVariableDefinition
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultVariableSpeciationCV
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultVariableNoDataValue
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultUnitsTypeCV
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultUnitsAbbreviation
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultUnitsName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultUnitsLink
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultCVUnitsType
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultCVTerm
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string DefaultCVName
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
