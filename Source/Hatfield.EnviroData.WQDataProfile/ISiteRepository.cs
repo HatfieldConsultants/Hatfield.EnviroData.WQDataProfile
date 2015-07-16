3773f4e052aa61f785abe3253eee5916190a83f3
gvassas <guillaume.vassas@gmail.com>
2015-07-16 15:50:45 -0700

    ISiterepository needed to inherit from Irepository

--- Source/Hatfield.EnviroData.WQDataProfile/ISiteRepository.cs
+++ Source/Hatfield.EnviroData.WQDataProfile/ISiteRepository.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Hatfield.EnviroData.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Hatfield.EnviroData.WQDataProfile
 {
-    public interface ISiteRepository
+    public interface ISiteRepository : IRepository<Site>
     {
     }
 }
