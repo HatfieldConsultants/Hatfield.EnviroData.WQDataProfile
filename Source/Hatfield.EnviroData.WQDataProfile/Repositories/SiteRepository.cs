8fe9bc1a7ec4688235ca23f7521111507d214b6a
Daniel <luzhanxun.daniel@gmail.com>
2015-07-16 15:07:37 -0700

    Add site repository
    Add site repository
    Update assembly info

---
+++ Source/Hatfield.EnviroData.WQDataProfile/Repositories/SiteRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Hatfield.EnviroData.Core;
+using Hatfield.EnviroData.Core.Repositories;
+
+namespace Hatfield.EnviroData.WQDataProfile.Repositories
+{
+    public class SiteRepository : Repository<Site>, ISiteRepository
+    {
+        public SiteRepository(IDbContext dbContext)
+            : base(dbContext)
+        {
+
+        }
+    }
+}
