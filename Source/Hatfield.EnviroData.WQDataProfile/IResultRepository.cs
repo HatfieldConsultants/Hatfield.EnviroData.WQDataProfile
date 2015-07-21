51546ecfc426aab52b90a30ddf54ddff55bf8979
gvassas <guillaume.vassas@gmail.com>
2015-07-21 10:29:59 -0700

    Added a result repository

---
+++ Source/Hatfield.EnviroData.WQDataProfile/IResultRepository.cs
@@ -0,0 +1,12 @@
+﻿using Hatfield.EnviroData.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hatfield.EnviroData.WQDataProfile
+{
+    public interface IResultRepository: IRepository<Result>
+    {
+    }
+}
---
+++ Source/Hatfield.EnviroData.WQDataProfile/Repositories/ResultRepository.cs
@@ -0,0 +1,18 @@
+﻿using Hatfield.EnviroData.Core;
+using Hatfield.EnviroData.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hatfield.EnviroData.WQDataProfile.Repositories
+{
+    public class ResultRepository : Repository<Result>, IResultRepository
+    {
+        public ResultRepository(IDbContext dbContext)
+            : base(dbContext)
+        {
+
+        }
+    }
+}
