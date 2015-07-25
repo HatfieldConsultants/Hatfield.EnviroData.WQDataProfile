09be46d9a583f0abdf3b42d0563a22964fa18b11
Daniel <luzhanxun.daniel@gmail.com>
2015-07-24 17:09:00 -0700

    Add WQDataRepository
    Add WQDataRepository

---
+++ Source/Hatfield.EnviroData.WQDataProfile/IWQDataRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Hatfield.EnviroData.Core;
+using Hatfield.EnviroData.Core.Repositories;
+
+namespace Hatfield.EnviroData.WQDataProfile
+{
+    public interface IWQDataRepository : IRepository<Hatfield.EnviroData.Core.Action>
+    {
+        IEnumerable<Hatfield.EnviroData.Core.Action> GetAllWQSampleDataActions();
+        IEnumerable<Hatfield.EnviroData.Core.Action> GetAllWQAnalyteDataActions();
+
+        int SaveChanges();
+    }
+}
---
+++ Source/Hatfield.EnviroData.WQDataProfile/Repositories/WQDataRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Hatfield.EnviroData.Core;
+using Hatfield.EnviroData.Core.Repositories;
+
+namespace Hatfield.EnviroData.WQDataProfile.Repositories
+{
+    public class WQDataRepository : Repository<Core.Action>, IWQDataRepository
+    {
+        public WQDataRepository(IDbContext dbContext)
+            : base(dbContext)
+        {
+
+        }
+
+        public IEnumerable<Core.Action> GetAllWQAnalyteDataActions()
+        {
+            throw new NotImplementedException("Get all WQ analyte data actions function is not implemented");
+        }
+
+        public IEnumerable<Core.Action> GetAllWQSampleDataActions()
+        {
+            throw new NotImplementedException("Get all WQ sample data actions function is not implemented");
+        }
+
+
+        public int SaveChanges()
+        {
+            return _dbContext.SaveChanges();
+        }
+    }
+}
