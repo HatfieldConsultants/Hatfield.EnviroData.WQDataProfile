9f61e2399a5438a74f7ca139323f4e8de305533d
Daniel <luzhanxun.daniel@gmail.com>
2015-07-27 16:14:06 -0700

    Add DataVersioningHelper
    Update assembly info to 1.0.0.4
    Add Place holder for IDataVersionHelper

---
+++ Source/Hatfield.EnviroData.WQDataProfile/IDataVersioningHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Hatfield.EnviroData.Core;
+
+namespace Hatfield.EnviroData.WQDataProfile
+{
+    public interface IDataVersioningHelper
+    {
+        Hatfield.EnviroData.Core.Action CloneDataToNewVersion(Hatfield.EnviroData.Core.Action previousAction);
+    }
+}
