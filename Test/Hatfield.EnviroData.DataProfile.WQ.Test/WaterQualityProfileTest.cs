d6a926c4a43cc39a180f28e35428bd61cd1bc5a8
Daniel <luzhanxun.daniel@gmail.com>
2016-06-27 17:00:04 -0700

    set up unit test project

---
+++ Test/Hatfield.EnviroData.DataProfile.WQ.Test/WaterQualityProfileTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using Hatfield.EnviroData.Core;
+using Hatfield.EnviroData.DataProfile.WQ.Models;
+
+
+namespace Hatfield.EnviroData.DataProfile.WQ.Test
+{
+    [TestFixture]
+    public class WaterQualityProfileTest
+    {
+        [Test]
+        public void TestSiteQuery()
+        {
+            var siteDb = new InMemoryDatabaseGenerator().CreateTestDbContext();
+
+            var dataProfile = new WaterQualityDataProfile(siteDb);
+
+            //var actualSites = dataProfile.GetAllSites();
+
+            Assert.Throws<NotImplementedException>(() => dataProfile.GetAllSites());
+        }
+    }
+}
