a17b747f9442788a61be6beba234ccf0dd798d88
unknown <ssunani@HF-43F3RW1.HATCON.local>
2016-06-30 15:21:53 -0700

    Added Site Query test

--- Test/Hatfield.EnviroData.DataProfile.WQ.Test/WaterQualityProfileIntegrationTest.cs
+++ Test/Hatfield.EnviroData.DataProfile.WQ.Test/WaterQualityProfileIntegrationTest.cs
@@ -33,8 +33,21 @@ namespace Hatfield.EnviroData.DataProfile.WQ.Test
 
         [Test]
         public void SiteQueryTest()
-        {            
+        {
             var sites = _wqDataProfile.GetAllSites();
+            var arraySites = sites.ToArray();
+
+            double[] Longitudes = { 123.1207, 123.1207, 111.1111, 123.1207 };
+
+            string[] Names = { "C1", "C2", null, "S01" };
+
+            for (int i = 0; i < arraySites.Length; i++)
+            {
+                Assert.AreEqual(Names[i], arraySites[i].Name);
+                Assert.AreEqual(Longitudes[i], arraySites[i].Longitude);
+                Assert.AreEqual(49.2827, arraySites[i].Latitude);
+            }
+
             Assert.NotNull(sites);
             Assert.AreEqual(4, sites.Count());
         }
